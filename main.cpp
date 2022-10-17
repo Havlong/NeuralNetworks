5e912080bfd208a57cf3bb7f483c86008a9a8adf
havlong <grishin_evgenii@inbox.ru>
2022-10-17 17:02:43 +0300

    added digit printing function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include "Network.hpp"
 #include "MNISTReader.hpp"
 
+void printDigit(const std::vector<double> &digit) {
+    for (int i = 0; i < 30; ++i) {
+        std::cout << '_';
+    }
+    std::cout << '\n';
+    for (int i = 0; i < 28; ++i) {
+        std::cout << '|';
+        for (int j = 0; j < 28; ++j) {
+            if (digit[i * 28 + j] < 0.3)
+                std::cout << ' ';
+            else
+                std::cout << '*';
+        }
+        std::cout << "|\n";
+    }
+    std::cout << '|';
+    for (int i = 0; i < 28; ++i) {
+        std::cout << '_';
+    }
+    std::cout << '|' << std::endl;
+}
+
 int main() {
 
     auto [trainingData, testData] = MNISTReader::readDataSet("../MNIST/train-images.idx3-ubyte",
@@ -13,10 +35,15 @@ int main() {
     deepNet.sgd(trainingData, testData, 30, 10, 0.05, true);
 
     std::cout << "Prediction on random data from test dataset:\n";
+
+    srandom(time(nullptr));
     int i = static_cast<int>(random() % testData.size());
+
     activation output = deepNet.feedForward(testData[i].first);
     std::cout << "Predicted: " << std::max_element(output.begin(), output.end()) - output.begin() << '\n';
-    std::cout << "True Digit was: " << testData[i].second << std::endl;
+    std::cout << "True Digit was: " << testData[i].second << std::endl << std::endl;
+
+    printDigit(testData[i].first);
 
     return 0;
 }
